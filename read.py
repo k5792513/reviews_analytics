14b8eed219c6d8fb18b0aba43711c6ec2ce59824
Nick Huang <k5792513@yahoo.com.tw>
2019-11-12 22:51:15 +0800

    calu avg length of reviews

--- read.py
+++ read.py
@@ -6,7 +6,9 @@ with open('reviews.txt', 'r') as f:
 		count += 1 #count = count + 1
 		if count % 1000 == 0: # %是餘數
 			print(len(data))
-print(len(data))
-print(data[0])
-print('------------------------------')
-print(data[1])
+print('終於印完了！總共有', len(data), '筆資料')
+
+sun_len = 0
+for line in data:
+	sun_len = sun_len + len(line)
+print('留言的平均長度是', sun_len/1000000)
