a3972b21fd03a042212f3b2ca412ef2870d6e055
Nick Huang <k5792513@yahoo.com.tw>
2019-11-13 22:11:08 +0800

    add filtering 2

--- read.py
+++ read.py
@@ -8,10 +8,12 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('終於印完了！總共有', len(data), '筆資料')
 
+
 sun_len = 0
 for line in data:
 	sun_len = sun_len + len(line)
 print('留言的平均長度是', sun_len/1000000)
+
 
 x = 0
 for line in data:
@@ -19,8 +21,23 @@ for line in data:
 		x += 1
 print('一共有', x, '則留言長度小於100')
 
-new = []
+
+new = [] #製造一個新清單，即是new
 for line in data:
 	if len(line) < 100:
 		new.append(line)
 print('一共有', len(new), '則留言長度小於100')
+
+
+x = 0
+for line in data:
+	if 'good' in line:
+		x += 1
+print('一共有', x, '則留言提到good')
+
+
+good = []
+for line in data:
+	if 'good' in line:
+		good.append(line)
+print('一共有', len(good), '則留言提到good')
