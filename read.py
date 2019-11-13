5e639ab79b2f1c5d00478db946f52b7a87c3cb0f
Nick Huang <k5792513@yahoo.com.tw>
2019-11-13 21:51:25 +0800

    add filtering

--- read.py
+++ read.py
@@ -12,3 +12,15 @@ sun_len = 0
 for line in data:
 	sun_len = sun_len + len(line)
 print('留言的平均長度是', sun_len/1000000)
+
+x = 0
+for line in data:
+	if len(line) < 100:
+		x += 1
+print('一共有', x, '則留言長度小於100')
+
+new = []
+for line in data:
+	if len(line) < 100:
+		new.append(line)
+print('一共有', len(new), '則留言長度小於100')
